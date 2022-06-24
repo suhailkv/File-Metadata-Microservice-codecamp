2c5e90373e15ff73f0bb6a08676a2709e6b02c4e
suhailkv <>
2022-06-24 13:34:32 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -7,9 +8,18 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const upload = multer({ dest: "src/uploads" });
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+app.post('/api/fileanalyse',upload.single('upfile'),async(req,res)=>{
+  res.json({
+    name:req.file.originalname,
+      type:req.file.mimetype,
+    size:req.file.size
+  })
+  
+})
 
 
 
